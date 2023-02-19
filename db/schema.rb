22dbccd01a857afd637143cab81c29beb60c1eef
bachikori <ya.7117.goo@gmail.com>
2023-02-19 12:25:18 +0900

    mariadb-client追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_06_140511) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "tasks", force: :cascade do |t|
+  create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.integer "status"
